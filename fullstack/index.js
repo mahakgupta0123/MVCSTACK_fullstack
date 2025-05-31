fe9d9b1da6ac13bc778b1335b5ab0806af88d819
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-05-31 12:21:53 +0530

    added index route

--- fullstack/index.js
+++ fullstack/index.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 8080
 const mongoose = require('mongoose')
 const listing = require('./models/listings.js')
+const path=require("path")
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.set("views",path.join(__dirname,"/views"));
+
 
 main()
   .then(res => {
@@ -14,27 +20,24 @@ async function main () {
   await mongoose.connect('mongodb://127.0.0.1:27017/airbnb')
 }
 
-app.get('/testing', (req, res) => {
-  const listing1 = new listing({
-    title: 'verre di wedding 2',
-    description: 'a luxury hotel to host private parties',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGpC6N4fsMlT85a8QBOc1aqztKs0CL3ugRKg&s',
-    price: 250000,
-    location: 'delhi',
-    country: 'india'
-  })
+// app.get('/testing', (req, res) => {
+//   const listing1 = new listing({
+//     title: 'verre di wedding 2',
+//     description: 'a luxury hotel to host private parties',
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGpC6N4fsMlT85a8QBOc1aqztKs0CL3ugRKg&s',
+//     price: 250000,
+//     location: 'delhi',
+//     country: 'india'
+//   })
 
-  listing1
-    .save()
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+//     res.send("saved successfully")
+// })
 
-    res.send("saved successfully")
+app.get("/listings", async (req,res)=>{
+    let datas= await listing.find();
+    res.render("index.ejs",{datas})
+    // console.log(datas);
 })
 
 app.get('/', (req, res) => {
