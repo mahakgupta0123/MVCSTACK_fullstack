feb06c11b5147cae60e75922d96860bfd3d46a79
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-06-06 13:49:23 +0530

    added show route

--- fullstack/index.js
+++ fullstack/index.js
@@ -3,12 +3,11 @@ const app = express()
 const port = 8080
 const mongoose = require('mongoose')
 const listing = require('./models/listings.js')
-const path=require("path")
-app.set("view engine","ejs")
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.set("views",path.join(__dirname,"/views"));
-
+const path = require('path')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, '/views'))
 
 main()
   .then(res => {
@@ -34,15 +33,32 @@ async function main () {
 //     res.send("saved successfully")
 // })
 
-app.get("/listings", async (req,res)=>{
-    let datas= await listing.find();
-    res.render("index.ejs",{datas})
-    // console.log(datas);
+app.get('/listings', async (req, res) => {
+  let datas = await listing.find()
+  res.render('index.ejs', { datas })
+  // console.log(datas);
 })
 
-app.get('/', (req, res) => {
-  res.send('hello, root')
+// app.get('/', (req, res) => {
+//   res.send('hello, root')
+// })
+
+app.get('/listings/new', (req, res) => {
+  res.render('form.ejs')
 })
+
+app.post("/listings",(req,res)=>{
+  let {title,description,price,location}=req.body;
+  console.log(req.body)
+  res.redirect("/listings")
+})
+
+app.get('/listings/:id', async (req, res) => {
+  let { id } = req.params
+  let data = await listing.findById(id)
+  res.render('listing.ejs', { data })
+})
+
 
 app.listen(port, (req, res) => {
   console.log('server is running')
