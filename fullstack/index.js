8921d1716e280d9be06ba6825c9202eaa265a688
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-06-08 07:55:15 +0530

    added styling

--- fullstack/index.js
+++ fullstack/index.js
@@ -3,14 +3,16 @@ const app = express()
 const port = 8080
 const mongoose = require('mongoose')
 const listing = require('./models/listings.js')
+const engine= require("ejs-mate")
+app.engine('ejs', engine);
 const path = require('path')
 var methodOverride = require('method-override')
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
-
 main()
   .then(res => {
     console.log('database is connected')
