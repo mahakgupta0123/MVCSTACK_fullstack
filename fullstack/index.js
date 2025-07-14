e5d0696564e8afd2381af5fb90c949fa50bd87ea
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-07-14 13:42:17 +0530

    improve the deployment code

--- fullstack/index.js
+++ fullstack/index.js
@@ -20,6 +20,7 @@ app.use(express.json())
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
+const ExpressError = require('./utils/ExpressError');
 main()
   .then(res => {
     console.log('database is connected')
